718217e3e2b40b2e961c3095c3e360492ca2ff4b
Itzhak Dangoor <itzhak.dangoor@gmail.com>
2018-08-31 13:58:16 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <p>I am getting set and ready to make this Hackathon great and enjoyable!</p>
+    <p>Good luck and have fun.
+      Itsik Dangoor.
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
